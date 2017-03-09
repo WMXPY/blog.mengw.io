fff7890b259aa6f686480876a16a06c14b49fbb8
Meng Wang <pc@wmpcxpy.com>
2017-03-09 09:50:05 -0600

    int

---
+++ OJ/Medium/IntegertoRoman/IntegertoRoman.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var intToRoman = function (num) {
+    var lookup = {
+        M: 1000,
+        CM: 900,
+        D: 500,
+        CD: 400,
+        C: 100,
+        XC: 90,
+        L: 50,
+        XL: 40,
+        X: 10,
+        IX: 9,
+        V: 5,
+        IV: 4,
+        I: 1
+    };
+    var romanStr = "";
+    for (var i in lookup) {
+        while (num >= lookup[i]) {
+            romanStr += i;
+            num -= lookup[i];
+        }
+    }
+    return romanStr;
+};
+console.log(intToRoman(476));
