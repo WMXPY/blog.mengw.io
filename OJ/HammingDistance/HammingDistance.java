f5f3a2f524f38cf965fd9e95a0dd6a0b3a4148cb
wmxpy <water.wang@live.com>
2017-03-08 20:30:17 -0600

    oj

---
+++ OJ/HammingDistance/HammingDistance.java
@@ -0,0 +1,6 @@
+public class Solution {
+    public int hammingDistance(int x, int y) {
+        char[] ax = x.toCharArray();
+        char[] ay = t.toCharArray();
+    }
+}
--- OJ/HammingDistance/HammingDistance.js
+++ OJ/HammingDistance/HammingDistance.js
@@ -4,8 +4,9 @@
  * @return {number}
  */
 var hammingDistance = function (x, y) {
-    x = x.toString(2);
-    y = y.toString(2);
-    console.log(x + 1);
+    x = parseInt(x.toString(2));
+    y = parseInt(y.toString(2));
+    // console.log(x + 1);
+
 };
 hammingDistance(2, 4);
