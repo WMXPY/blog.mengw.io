43e9631f4b392b129ed5b04429c40d311178b92e
wmxpy <water.wang@live.com>
2017-03-08 20:19:49 -0600

    HammingDistance

---
+++ OJ/HammingDistance/HammingDistance.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} x
+ * @param {number} y
+ * @return {number}
+ */
+var hammingDistance = function (x, y) {
+    x = x.toString(2);
+    y = y.toString(2);
+    console.log(x + 1);
+};
+hammingDistance(2, 4);
