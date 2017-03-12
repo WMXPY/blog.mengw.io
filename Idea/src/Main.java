e41f9def43ddc743f0dbc8d962c608e32510b66b
wmxpy <wm@wmpcxpy.com>
2017-03-11 19:46:18 -0600

    main

--- Idea/src/Main.java
+++ Idea/src/Main.java
@@ -1,27 +1,24 @@
 public class Main {
 
     public static void main(String[] args) {
-//        int[] input = {3,2,4};
-//        int[] lists = twoSum(input,6);
-//        for (int i = 0; i < lists.length; i++) {
-//            System.out.println(lists[i]);
-//        }
-    }
-    public static int[] twoSum(int[] nums, int target) {
-        int nums_length = nums.length;
-        for (int i = 0; i < nums_length; i++) {
-            for (int j = nums_length - 1; j >=0; j--) {
-                if (nums[i] + nums[j] == target && i!=j) {
-                    int[] re = { i, j };
-                    return re;
+        int[] a = new int[101];
+        int i, j = 2;
+
+        while (j < 101) {
+            if (a[j] == 0) {
+                for (i = j + 1; i < 101; i++) {
+                    if (i % j == 0) {
+                        a[i] = 1;
+                    }
                 }
             }
+            j++;
         }
-        int[] re = {};
-        return re;
-    }
-    public static boolean containsNearbyDuplicate(int[] nums, int k) {
 
-    return false;
+        for (int k = 0; k < 101; k++) {
+            if (k >= 2 && a[k] == 0) {
+                System.out.println(k);
+            }
+        }
     }
 }
--- Idea/src/Solutions.java
+++ Idea/src/Solutions.java
@@ -2,5 +2,22 @@
  * Created by WMPCXPY on 2017/3/11.
  */
 public class Solutions {
-
+    public static int[] twoSum(int[] nums, int target) {
+        //        int[] input = {3,2,4};
+        //        int[] lists = twoSum(input,6);
+        //        for (int i = 0; i < lists.length; i++) {
+        //            System.out.println(lists[i]);
+        //        }
+        int nums_length = nums.length;
+        for (int i = 0; i < nums_length; i++) {
+            for (int j = nums_length - 1; j >= 0; j--) {
+                if (nums[i] + nums[j] == target && i != j) {
+                    int[] re = {i, j};
+                    return re;
+                }
+            }
+        }
+        int[] re = {};
+        return re;
+    }
 }
